ba7bd48ee4c6fe2306b73a1b83eede376f5bf772
Андрей Миронов <avale1648@gmail.com>
2023-06-24 13:29:43 +0300

    Add Solution View Form

---
+++ SupervisoryService/SupervisoryServiceLibrary/SolutionViewForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SupervisoryServiceLibrary
+{
+    partial class SolutionViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelSolution = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelResponsible = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelSolution
+            // 
+            this.labelSolution.AutoSize = true;
+            this.labelSolution.Location = new System.Drawing.Point(218, 12);
+            this.labelSolution.Name = "labelSolution";
+            this.labelSolution.Size = new System.Drawing.Size(60, 15);
+            this.labelSolution.TabIndex = 2;
+            this.labelSolution.Text = "Решение:";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(12, 215);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(38, 15);
+            this.labelDate.TabIndex = 3;
+            this.labelDate.Text = "Дата: ";
+            // 
+            // labelResponsible
+            // 
+            this.labelResponsible.AutoSize = true;
+            this.labelResponsible.Location = new System.Drawing.Point(12, 235);
+            this.labelResponsible.Name = "labelResponsible";
+            this.labelResponsible.Size = new System.Drawing.Size(94, 15);
+            this.labelResponsible.TabIndex = 4;
+            this.labelResponsible.Text = "Ответственный:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 255);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(49, 15);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Cтатус: ";
+            // 
+            // SolutionViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelResponsible);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelSolution);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SolutionViewForm";
+            this.Text = "SolutionViewForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label labelSolution;
+        private Label labelDate;
+        private Label labelResponsible;
+        private Label labelStatus;
+    }
+}
---
+++ SupervisoryService/SupervisoryServiceLibrary/SolutionViewForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupervisoryServiceLibrary
+{
+    public partial class SolutionViewForm : Form
+    {
+        public SolutionViewForm()
+        {
+            InitializeComponent();
+        }
+        public SolutionViewForm(Solution solution)
+        {
+            InitializeComponent();
+            labelSolution.Text += $" {solution.Title}\n{solution.Description}";
+            labelDate.Text += $" {solution.Date.ToShortDateString()}";
+            labelResponsible.Text += $" {solution.Responsible}";
+            labelStatus.Text += $" {solution.Status}";
+        }
+    }
+}
