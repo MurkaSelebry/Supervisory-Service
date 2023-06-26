4fd34ccba5ce3afeadcdadf05189d25fc9e49efe
Андрей Миронов <avale1648@gmail.com>
2023-06-26 20:47:31 +0300

    Fix GroupBox Enabled and Button View Visibility

--- MapTest/MapTest/Form1.cs
+++ MapTest/MapTest/Form1.cs
@@ -6,25 +6,6 @@ namespace MapTest
         public Form1()
         {
             InitializeComponent();
-            webBrowser1.ScriptErrorsSuppressed = true;
-        }
-        private void buttonFind_Click(object sender, EventArgs e)
-        {
-            string state = textBoxState.Text;
-            string city = textBoxCity.Text;
-            string street = textBoxStreet.Text;
-            string zipCode = textBoxZip.Text;
-            StringBuilder querryAdress = new StringBuilder();
-            querryAdress.Append("http://google.com/maps?q=");
-            if (!string.IsNullOrEmpty(state))
-                querryAdress.Append(state + "," + "+");
-            if (!string.IsNullOrEmpty(city))
-                querryAdress.Append(city + "," + "+");
-            if (!string.IsNullOrEmpty(street))
-                querryAdress.Append(street + "," + "+");
-            if (!string.IsNullOrEmpty(zipCode))
-                querryAdress.Append(zipCode + "," + "+");
-            webBrowser1.Navigate(querryAdress.ToString());
         }
     }
 }
